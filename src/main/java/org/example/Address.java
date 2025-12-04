48002c9630650b7e9c10b9ad060a698b6bddd7b3
2544628 <b.e.cunanan28@gmail.com>
2025-12-04 09:17:39 -0500

    Add the initial assignment file for the project.

---
+++ src/main/java/org/example/Address.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    public Province province;
+    private String postalCode;
+
+    public enum Province {
+        AB,
+        BC,
+        MB,
+        NB,
+        NL,
+        NS,
+        ON,
+        PE,
+        QC,
+        SK
+    }
+
+
+}
---
+++ src/main/java/org/example/Assignment.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private ArrayList<Integer> scores;
+
+    public static int nextId = 0;
+}
