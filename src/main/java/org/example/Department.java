2a18be6d565014627b66cca8e506a5819743d361
2544628 <b.e.cunanan28@gmail.com>
2025-12-07 21:50:53 -0500

    Added isDepartmentNameValid method and constructor to Department

--- src/main/java/org/example/Department.java
+++ src/main/java/org/example/Department.java
@@ -13,4 +13,23 @@ public class Department {
     @Setter private String departmentName;
 
     public static int nextid = 0;
+
+    public static boolean isDepartmentNameValid(String departmentName) {
+        String idxLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWWXYZ";
+        String idxSpace = " ";
+
+        if (!departmentName.contains(idxLetters) || !departmentName.contains(idxSpace)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName) == true) {
+            this.departmentName = departmentName;
+        } else {
+            this.departmentName = null;
+        }
+    }
 }
