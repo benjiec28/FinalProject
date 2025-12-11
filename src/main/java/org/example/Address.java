aedc5ec90c58b9d6369fb7fb222fa1a743afd2fa
2544628 <b.e.cunanan28@gmail.com>
2025-12-11 00:49:52 -0500

    Fixed isPostalCodeValid helper method

--- src/main/java/org/example/Address.java
+++ src/main/java/org/example/Address.java
@@ -37,33 +37,26 @@ public class Address {
         String idxChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String idxDigit = "1234567890";
 
-        char idx0 = postalCode.charAt(0);
-        String idx0Value = String.valueOf(idx0);
-        char idx1 = postalCode.charAt(1);
-        String idx1Value = String.valueOf(idx0);
-        char idx2 = postalCode.charAt(2);
-        String idx2Value = String.valueOf(idx0);
-        char idx3 = postalCode.charAt(3);
-        String idx3Value = String.valueOf(idx0);
-        char idx4 = postalCode.charAt(4);
-        String idx4Value = String.valueOf(idx0);
-        char idx5 = postalCode.charAt(5);
-        String idx5Value = String.valueOf(idx0);
+        for (int i = 0; i < postalCode.length(); i++) {
+            if (i % 2 == 0 && postalCode.contains(idxChar)) {
+                return true;
+            }
 
-        if (idx0Value.contains(idxChar) || idx2Value.contains(idxChar) || idx4Value.contains(idxChar) || idx1Value.contains(idxDigit) || idx3Value.contains(idxDigit) || idx5Value.contains(idxDigit)) {
-            return true;
+            if (i % 2 == 1 && postalCode.contains(idxDigit)) {
+                return true;
+            }
         }
 
         return false;
     }
 
     public Address(int streetNo, String street, String city, Province province, String postalCode) {
-        if (isPostalCodeValid(postalCode) == true) {
+        if (isPostalCodeValid(postalCode)) {
             this.streetNo = streetNo;
             this.street = street;
             this.city = city;
             this.province = province;
-            this.postalCode = postalCode;
+            this.postalCode = postalCode.toUpperCase();
         } else {
             this.streetNo = 0;
             this.street = null;
