9107e60f21d4bc62f5634aa54a004dea34fb3c3f
2544628 <b.e.cunanan28@gmail.com>
2025-12-11 03:14:54 -0500

    Added the toTitleCase method

--- src/main/java/org/example/Util.java
+++ src/main/java/org/example/Util.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Util {
     public static String toTitleCase(String str) {
+        String[] words = str.split(" ");
 
+        for (String word : words) {
+            if (word == null) {
+                return null;
+            }
+
+            int spaceIdx = word.indexOf(" ");
+            String firstLetter = word.substring(0, 1).toUpperCase();
+            String restOfName = word.substring(1, spaceIdx).toLowerCase();
+
+            return firstLetter + restOfName + " ";
+        }
+
+        return Arrays.toString(words);
     }
 }
