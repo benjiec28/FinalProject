9c90e69127a1b268ec2fecd4f58585c75c793fb2
2544628 <b.e.cunanan28@gmail.com>
2025-12-13 02:57:23 -0500

    Fixed toTitleCase()

--- src/main/java/org/example/Util.java
+++ src/main/java/org/example/Util.java
@@ -4,20 +4,19 @@ import java.util.Arrays;
 
 public class Util {
     public static String toTitleCase(String str) {
-        String[] words = str.split(" ");
-
-        for (String word : words) {
-            if (word == null) {
-                return null;
-            }
-
-            int spaceIdx = word.indexOf(" ");
-            String firstLetter = word.substring(0, 1).toUpperCase();
-            String restOfName = word.substring(1, spaceIdx).toLowerCase();
-
-            return firstLetter + restOfName + " ";
+        if (str == null || str.isEmpty()) {
+            return str;
         }
 
-        return Arrays.toString(words);
+        String[] words = str.split(" ");
+        String finalStr = "";
+
+        for (String word : words) {
+            String firstLetter = word.substring(0, 1).toUpperCase();
+            String restOfName = word.substring(1).toLowerCase();
+            finalStr = finalStr + firstLetter + restOfName + " ";
+        }
+
+        return finalStr.trim();
     }
 }
