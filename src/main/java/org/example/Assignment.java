d32811003d5c66de8cc2a146615879335cd046cd
2544628 <b.e.cunanan28@gmail.com>
2025-12-13 02:34:27 -0500

    Added generateRandomScore() method

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -26,6 +26,22 @@ public class Assignment {
 
     public static void generateRandomScore() {
         Random random = new Random();
+        int randomValue = random.nextInt(0,10 + 1);
+        int randomScore = 0;
+
+        if (randomValue == 0) {
+            randomScore = random.nextInt(0,60 + 1);
+        } else if (randomValue == 1 || randomValue == 2) {
+            randomScore = random.nextInt(60, 70 + 1);
+        }else if (randomValue == 3 || randomValue == 4) {
+            randomScore = random.nextInt(70, 80 + 1);
+        }else if (randomValue == 5 || randomValue == 6 || randomValue == 7 || randomValue == 8) {
+            randomScore = random.nextInt(80, 90 + 1);
+        }else if (randomValue == 9 || randomValue == 10) {
+            randomScore = random.nextInt(90, 100 + 1);
+        }
+
+        scores.add(randomScore);
     }
 
     @Override
