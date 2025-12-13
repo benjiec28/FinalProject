85f1082198c3567be7d90cc440b09e4e65f2633b
2544628 <b.e.cunanan28@gmail.com>
2025-12-13 00:19:45 -0500

    Added isAssignmentWeightValid

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -4,6 +4,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.Array;
 import java.util.ArrayList;
 
 @Getter
@@ -18,18 +19,46 @@ public class Course {
     private ArrayList<Student> registeredStudents;
 
     public static int nextId = 0;
-    ..= nextId++;
+    public static String generateNewId() {
+        return String.format("%d", nextId++);};
 
     public static boolean isAssignmentWeightValid() {
+        int totalSum = 0;
+        ArrayList<Assignment> assignments = new ArrayList<>();
+        Assignment assignment = new Assignment();
 
+        for (int i = 0; i < assignments.size(); i++) {
+            assignments.get(i);
+            int weight = Assignment.getWeight;
+            int weightPercentage = weight / 100;
+            i *= weightPercentage;
+            totalSum += i;
+        }
+
+        if (totalSum == 100) {
+            return true;
+        }
+
+        return false;
     }
 
     public static boolean registerStudent(Student student) {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(0, student);
 
     }
 
     public static int[] calcStudentsAverage() {
-
+        int[] averages = new int[];
+        ArrayList<Assignment> assignments = new ArrayList<>();
+        int avg = 0;
+        for (int i = 0; i < averages.length; i++) {
+            assignments.get(i);
+            int weight = Assignment.getWeight;
+            int weightPercentage = weight / 100;
+            i *= weightPercentage;
+            avg += i;
+        }
     }
 
     public static boolean addAssignment(String assignmentName, double weight, int maxScore) {
@@ -37,7 +66,9 @@ public class Course {
     }
 
     public static void generateScores() {
-
+        System.out.printf("Course: %s\n", courseId);
+        System.out.print("                        Assignment01   Assignment02   Assignment03         Exam01         Exam02    Final Score\n");
+        System.out.printf(" Ethan Collins             %02d           %02d            %02d             %02d           %02d          %02d\n");
     }
 
     public static void displayScores() {
@@ -48,6 +79,6 @@ public class Course {
 
     }
 
-    // toString
+
 
 }
