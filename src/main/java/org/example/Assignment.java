4f4bb2a27f65af1c662819cf6f0662027d6c2bc4
2544628 <b.e.cunanan28@gmail.com>
2025-12-13 21:13:02 -0500

    Added constructor. Fixed generateRandomScore() and toString.

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -1,20 +1,30 @@
 package org.example;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Random;
 
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Assignment {
     private String assignmentId;
     private String assignmentName;
     private double weight;
-    private static ArrayList<Integer> scores;
+    private ArrayList<Integer> scores;
 
     public static int nextId = 0;
-    public static String generateNewId() {
-        return String.format("%d", nextId++);
+
+    public Assignment(String assignmentName, double weight) {
+        this.assignmentId = String.format("%d", nextId++);
+        this.assignmentName = assignmentName;
+        this.weight = weight;
     }
 
-    public static void calcAssignmentAvg() {
+    public void calcAssignmentAvg() {
         int sum = 0;
 
         for (int score : scores) {
@@ -24,21 +34,21 @@ public class Assignment {
         int avg = sum / scores.size();
     }
 
-    public static void generateRandomScore() {
+    public void generateRandomScore() {
         Random random = new Random();
-        int randomValue = random.nextInt(0,10 + 1);
+        int randomValue = random.nextInt(0,10);
         int randomScore = 0;
 
         if (randomValue == 0) {
-            randomScore = random.nextInt(0,60 + 1);
+            randomScore = random.nextInt(0,60);
         } else if (randomValue == 1 || randomValue == 2) {
-            randomScore = random.nextInt(60, 70 + 1);
+            randomScore = random.nextInt(60, 70);
         }else if (randomValue == 3 || randomValue == 4) {
-            randomScore = random.nextInt(70, 80 + 1);
+            randomScore = random.nextInt(70, 80);
         }else if (randomValue == 5 || randomValue == 6 || randomValue == 7 || randomValue == 8) {
-            randomScore = random.nextInt(80, 90 + 1);
+            randomScore = random.nextInt(80, 90);
         }else if (randomValue == 9 || randomValue == 10) {
-            randomScore = random.nextInt(90, 100 + 1);
+            randomScore = random.nextInt(90, 100);
         }
 
         scores.add(randomScore);
@@ -47,9 +57,9 @@ public class Assignment {
     @Override
     public String toString() {
         return "Assignment{" +
-                String.format("assignmentId= " + nextId++ + '\'' +
-                ", assignmentName= " + assignmentName + '\'' +
-                ", weight= " + weight +
+                "assignmentId='" + assignmentId + '\'' +
+                ", assignmentName='" + assignmentName + '\'' +
+                ", weight=" + weight +
                 '}';
     }
 }
