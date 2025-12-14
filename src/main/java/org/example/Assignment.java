f91708a2c03d85d534fc67d3d5bb09eb0f9a9572
2544628 <b.e.cunanan28@gmail.com>
2025-12-14 00:56:34 -0500

    Cleaned up generateRandomScore()

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -41,12 +41,16 @@ public class Assignment {
 
         if (randomValue == 0) {
             randomScore = random.nextInt(0,60);
+
         } else if (randomValue == 1 || randomValue == 2) {
             randomScore = random.nextInt(60, 70);
+
         }else if (randomValue == 3 || randomValue == 4) {
             randomScore = random.nextInt(70, 80);
+
         }else if (randomValue == 5 || randomValue == 6 || randomValue == 7 || randomValue == 8) {
             randomScore = random.nextInt(80, 90);
+
         }else if (randomValue == 9 || randomValue == 10) {
             randomScore = random.nextInt(90, 100);
         }
