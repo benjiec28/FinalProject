6566cefb0413886e3e38be06d8025b1fd1fb3153
2544628 <b.e.cunanan28@gmail.com>
2025-12-14 02:32:23 -0500

    Added isAssignmentWeightValid() and registerStudent()

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -16,78 +16,94 @@ public class Course {
     private double credits;
     private Department department;
     private ArrayList<Assignment> assignments;
-    public ArrayList<Student> registeredStudents;
-
+    private ArrayList<Student> registeredStudents;
+    private ArrayList<Double> finalScores;
 
     public static int nextId = 0;
-    public static String generateNewId() {
-        return String.format("C-%d-%d", Department.departmenId, nextId++);};
 
     public Course(String courseId, String courseName, double credits, Department department) {
-        this.courseId = courseId;
+        this.courseId = String.format("C-%d-%02d", department.getDepartmentId(), nextId++);
         this.courseName =  Util.toTitleCase(courseName);
         this.credits = credits;
         this.department = department;
     }
 
-    public static boolean isAssignmentWeightValid() {
-        int totalSum = 0;
-        ArrayList<Assignment> assignments = new ArrayList<>();
-        Assignment assignment = new Assignment();
+    /**
+     *
+     * @return
+     */
+    public boolean isAssignmentWeightValid() {
+        double totalSum = 0;
+        double totalWeight = 100;
 
-        for (int i = 0; i < assignments.size(); i++) {
-            assignments.get(i);
-            int weight = Assignment.Weight();
-            totalSum += i;
+        for (Assignment assignment : assignments) {
+            totalSum += assignment.getWeight();
         }
-
-        if (totalSum == 100) {
-            return true;
-        }
-
-        return false;
-    }
-
-    public static boolean registerStudent(Student student) {
-        ArrayList<Student> students = new ArrayList<>();
-        students.add(0, student);
+        return totalSum == totalWeight;
 
     }
 
+    /**
+     *
+     * @param student
+     * @return
+     */
+    public boolean registerStudent(Student student) {
+        if (registeredStudents.contains(student)) {
+            return false;
+        }
+
+        registeredStudents.add(student);
+
+        for (Assignment assignment : assignments) {
+            assignment.getScores().add(null);
+
+            finalScores.add(null);
+        }
+
+        return true;
+    }
+
+    /**
+     *
+     * @return
+     */
     public static int[] calcStudentsAverage() {
-        ArrayList<Student> registeredStudents = new ArrayList<>();
-        int size = registeredStudents.size();
-        ArrayList<Assignment> assignments = new ArrayList<>();
 
-        int[] avgArray = new int[size];
-        int avg = 0;
-
-        for (int i = 0; i < avgArray.length; i++) {
-            assignments.get(i);
-            int weight = Assignment.getWeight;
-            int weightPercentage = weight / 100;
-            i *= weightPercentage;
-            avg += i;
-        }
-
-        for ()
 
     }
 
+    /**
+     *
+     * @param assignmentName
+     * @param weight
+     * @param maxScore
+     * @return
+     */
     public static boolean addAssignment(String assignmentName, double weight, int maxScore) {
 
     }
 
+    /**
+     *
+     */
     public void generateScores() {
 
     }
 
+    /**
+     *
+     */
     public void displayScores() {
         System.out.printf("Course: %s\n", courseId);
         System.out.print("                        Assignment01   Assignment02   Assignment03         Exam01         Exam02    Final Score\n");
         System.out.printf(" Ethan Collins             %02d           %02d            %02d             %02d           %02d          %02d\n");
     }
 
+    /**
+     *
+     * @return
+     */
     public static String toSimplifiedString() {
 
     }
